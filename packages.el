624555dab4519dc9bb7f63c0bc65289fa840111a
Destin <destin@fong.io>
2020-01-30 19:50:55 -0500

    Added lsp-treemacs and pinned some packages

--- packages.el
+++ packages.el
@@ -24,11 +24,21 @@
 ;; (package! evil-collection)
 ;; (package! evil-magit)
 (package! feature-mode)
-(package! ember-mode)
-(package! easy-kill)
-(package! pretty-mode)
-(package! try)
-(package! org-jira)
-(package! rinari)
-(package! web-beautify)
-(package! json-mode)
+(package! ember-mode
+  :pin "34373dd9413986288b2709365b2725e9390ecf9e")
+(package! easy-kill
+  :pin "2a6309d98aa6b71df6bbbcdf15cab3187c521a6b")
+(package! pretty-mode
+  :pin "5154355e90fdd70d3647257280a89eeb725ef084")
+(package! try
+  :pin "8831ded1784df43a2bd56c25ad3d0650cdb9df1d")
+(package! org-jira
+  :pin "5123c29867e5da54d80e92f9a5a4259144451404")
+(package! rinari
+  :pin "134438af8fbdfa9c8077267c768d273a9792b484")
+(package! web-beautify
+  :pin "e1b45321d8c11b404b12c8e55afe55eaa7c84ee9")
+(package! json-mode
+  :pin "0e819e519ae17a2686e0881c4ca51fa873fa9b83")
+(package! lsp-treemacs
+  :pin "d097670f1d77e0c806b0094df4ebb575fb83ed87")
