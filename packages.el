029bf2290332e2a78ce74550cc87a3df4c786349
Destin <destin@fong.io>
2020-02-07 12:56:56 -0500

    Pinned packages and started custom company config

--- packages.el
+++ packages.el
@@ -1,20 +1,34 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-(package! rvm)
-(package! helm-dash)
-(package! avy)
-(package! coffee-mode)
-(package! auto-dictionary)
-(package! yasnippet-snippets)
-(package! multiple-cursors)
-(package! git-link)
-(package! ggtags)
-(package! flx)
-(package! tss)
-(package! tide)
-(package! typescript-mode)
-(package! deadgrep)
+(package! rvm
+  :pin "134497bc460990c71ab8fa75431156e62c17da2d")
+(package! helm-dash
+  :pin "7f853bd34da666f0e9a883011c80f451b06f6c59")
+(package! avy
+  :pin "cf95ba9582121a1c2249e3c5efdc51acd566d190")
+(package! coffee-mode
+  :pin "86ab8aae8662e8eff54d3013010b9c693b16eac5")
+(package! auto-dictionary
+  :pin "b364e08009fe0062cf0927d8a0582fad5a12b8e7")
+(package! yasnippet-snippets
+  :pin "612be838d2e50c3601c349d7842702d95e17682a")
+(package! multiple-cursors
+  :pin "b9b851a7670f4348f3a08b11ef12ed99676c8b84")
+(package! git-link
+  :pin "2fb0e4be8801e7d53e02fc7f14ec0f9a14586ba2")
+(package! ggtags
+  :pin "4d9000034d352cd983ae6626f560e434d0b3e9ca")
+(package! flx
+  :pin "51c7a6c70816e0d6093fc0614ee24f85f30755bf")
+(package! tss
+  :pin "81ac6351a2ae258fd0ebf916dae9bd5a179fefd0")
+(package! tide
+  :pin "1878a097fc41ee81c40c155022c8feaaf8bfaa6d")
+(package! typescript-mode
+  :pin "a0f2c3ebd4c7cde7f3c25f9cef12b679118d3351")
+(package! deadgrep
+  :pin "06764269582b2f844ae2a163d2fece8143b1c578")
 ;; DAP mode
 ;; (package! dap-mode)
 ;; (package! lsp-mode)
@@ -23,7 +37,8 @@
 ;; (package! evil-string-inflection)
 ;; (package! evil-collection)
 ;; (package! evil-magit)
-(package! feature-mode)
+(package! feature-mode
+  :pin "11ae1671629bfedaa553c7b819676d64eb320992")
 (package! ember-mode
   :pin "34373dd9413986288b2709365b2725e9390ecf9e")
 (package! easy-kill
@@ -42,3 +57,11 @@
   :pin "0e819e519ae17a2686e0881c4ca51fa873fa9b83")
 (package! lsp-treemacs
   :pin "d097670f1d77e0c806b0094df4ebb575fb83ed87")
+;; (package! highlight-symbol
+;; :pin "7a789c779648c55b16e43278e51be5898c121b3a")
+
+;; Completion
+(package! company
+  :pin "831371b1d418087da242ccb5cbe16a1e5ce6f5ca")
+(package! company-lsp
+  :pin "f921ffa0cdc542c21dc3dd85f2c93df4288e83bd")
