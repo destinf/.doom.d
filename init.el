64a4328f6344cba7aad16da010984b4cfe6a6602
Destin <destin@fong.io>
2020-05-12 11:41:37 -0400

    Fixing visuals and adding theme for terminal

--- init.el
+++ init.el
@@ -36,6 +36,7 @@
        (popup            ; tame sudden yet inevitable temporary windows
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
+       treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -44,7 +45,8 @@
        zen               ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
+       ;; (evil +everywhere); come to the dark side, we have cookies
+       evil              ; come to the dark side, we have cookies
        ;; file-templates    ; auto-snippets for empty files
        ;; fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
