af6d90dd11cae8b6aeb7cd426343c714e536a135
Destin Fong <dfong@ip-192-168-193-23.us-east-2.compute.internal>
2020-11-13 12:01:42 -0500

    Cleanup

--- packages.el
+++ packages.el
@@ -2,9 +2,6 @@
 ;;; ~/.doom.d/packages.el
 
 (package! rvm)
-;; (package! helm-dash)
-(package! avy)
-(package! coffee-mode)
 (package! auto-dictionary)
 (package! yasnippet-snippets)
 (package! multiple-cursors)
@@ -15,14 +12,6 @@
 (package! tide)
 (package! typescript-mode)
 (package! deadgrep)
-;; DAP mode
-;; (package! dap-mode)
-;; (package! lsp-mode)
-;; (package! lsp-ui)
-;; (package! company-lsp)
-;; (package! evil-string-inflection)
-;; (package! evil-collection)
-;; (package! evil-magit)
 (package! feature-mode)
 (package! ember-mode)
 (package! easy-kill)
@@ -35,17 +24,3 @@
 ;; (package! lsp-treemacs)
 ;; (package! highlight-symbol
 ;; :pin "7a789c779648c55b16e43278e51be5898c121b3a")
-
-;; Completion
-;; (package! company)
-;; (package! company-lsp)
-
-;; TODO Pin this package
-;; (package! flymake-ruby)
-
-;; TODO Pin this package
-;; (package! company-web)
-
-(package! impatient-mode)
-(package! kubernetes)
-(package! kubernetes-evil)
